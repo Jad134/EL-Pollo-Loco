42c75ec437d704f1c7426cab8b48f2e5cd0f3031
Jad El Nader <Jadelnader99@gmail.com>
2023-10-26 09:27:28 +0200

    Animate throwed Bottles

--- models/throwableObject.class.js
+++ models/throwableObject.class.js
@@ -1,25 +1,39 @@
 class ThrowableObject extends MovableObject {
+    IMAGES_BOTTLEROTATION = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    ]
+
 
 
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_BOTTLEROTATION)
         this.x = x;
-        this.y  = y;
+        this.y = y;
         this.height = 60;
-        this. width = 80;
+        this.width = 80;
         this.throw()
-        }
+    }
 
     throw() {
-        
+
         this.speedY = 30;
         this.applyGravity();
         setInterval(() => {
             this.x += 10;
         }, 25);
+        this.animate()
     }
 
+    animate() {
 
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_BOTTLEROTATION)
+        }, 50);
+    }
 
 }
 
